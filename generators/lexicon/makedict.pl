f2eccb14c644c6670ef17e85ec0e554702be7f38
Denis Moskowitz <denismm@gmail.com>
2018-03-01 20:14:40 -0500

    use RIKLANG instead of confusing environment vars

--- generators/lexicon/makedict.pl
+++ generators/lexicon/makedict.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 use strict;
-my $RIKCHIK_HOME = $ENV{'RIKCHIK_HOME'};
-my $RIKCHIK_SRC = $ENV{'RIKCHIK_SRC'};
-$ENV{'CLASSPATH'} = ".:$RIKCHIK_HOME/lib/saxon-9.1.jar";
-chdir "$RIKCHIK_HOME/language";
-system "/usr/bin/java net.sf.saxon.Transform $RIKCHIK_SRC/data/riklang.xml $RIKCHIK_SRC/generators/lexicon/riklang.xsl";
+my $RIKLANG = $ENV{'RIKLANG'} || $ENV{'RIKCHIK_SRC'} || $ENV{'RIKCHIK_HOME'}; 
+die unless $RIKLANG;
+$ENV{'CLASSPATH'} = ".:$RIKLANG/lib/saxon-9.1.jar";
+chdir "$RIKLANG/language";
+system "/usr/bin/java net.sf.saxon.Transform $RIKLANG/data/riklang.xml $RIKLANG/generators/lexicon/riklang.xsl";
