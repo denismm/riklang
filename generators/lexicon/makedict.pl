69cd2ff5b569f4bddf74634b9c8ae35439f243cd
Denis Moskowitz <dmm@suberic.net>
2018-03-08 19:48:59 -0500

    fix riklang/rikhome issues

--- generators/lexicon/makedict.pl
+++ generators/lexicon/makedict.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/perl
 use strict;
-my $RIKLANG = $ENV{'RIKLANG'} || $ENV{'RIKCHIK_SRC'} || $ENV{'RIKCHIK_HOME'}; 
+my $RIKLANG = $ENV{'RIKLANG'} || $ENV{'RIKCHIK_SRC'};
+my $RIKHOME = $ENV{'RIKCHIK_HOME'};
 die unless $RIKLANG;
 $ENV{'CLASSPATH'} = ".:$RIKLANG/lib/saxon-9.1.jar";
-chdir "$RIKLANG/language";
+chdir "$RIKHOME/language";
 system "/usr/bin/java net.sf.saxon.Transform $RIKLANG/data/riklang.xml $RIKLANG/generators/lexicon/riklang.xsl";
