089b9967b9a64d9b80519152a90143d63ca65d38
Denis Moskowitz <denismm@gmail.com>
2018-04-05 20:34:33 -0400

    denormalize and store corpus entries

--- parsers/corpus_search/read_corpus.py
+++ parsers/corpus_search/read_corpus.py
@@ -2,6 +2,7 @@
 import yaml
 import os
 import sys
+import copy
 corpus_source = "../../data/corpus"
 corpus_walk = os.walk(corpus_source)
 corpus_files = []
@@ -10,11 +11,33 @@ for location in corpus_walk:
         if filename.endswith('.yaml'):
             corpus_files.append(location[0] + '/' + filename)
 
+corpus = {} # ?
+
+def insert_entry(id, entry):
+    if id.startswith(corpus_source):
+        id = id[len(corpus_source):]
+    if id.startswith('/'):
+        id = id[1:]
+    corpus[id] = entry
+    print "%s ok" % id
+
 for filename in corpus_files:
     with open(filename, 'r') as f:
         data = f.read()
+    doc_i = 0
     for structure in yaml.load_all(data):
-        print filename + ": ok"
         # denormalize
-        # store
-        # iterate through titles etc
+        defaults = {
+            k: v for (k, v) in structure.iteritems() if not isinstance(v, list)
+        }
+        if 'utterance' in structure:
+            utter_i = 0
+            for utterance in structure['utterance']:
+                entry = copy.copy(defaults)
+                for (k, v) in utterance.iteritems():
+                    entry[k] = v
+                insert_entry("%s:%d:%d" % (filename, doc_i, utter_i), entry)
+                utter_i += 1
+        else:
+            insert_entry("%s:%d:%d" % (filename, doc_i, 0), defaults)
+        doc_i += 1
