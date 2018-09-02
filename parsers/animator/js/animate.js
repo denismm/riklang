f42380984690173cd5135250f035f1961467d250
Marc Moskowitz <marc@suberic.net>
2018-09-02 09:56:08 -0400

    Mostly completing #19 by getting animation working.

--- parsers/animator/js/animate.js
+++ parsers/animator/js/animate.js
@@ -12,9 +12,115 @@ function getDivisions(){
     return 4;
 }
 
+//given an elapsed number of rockspans, a set of words, and a context, draw the correct fram in that context.
+function frame(rsecs, words){
+    var whole = Math.floor(rsecs);
+    var remainder = rsecs - whole;
+    var index;
+    if (remainder < .5){
+	index = whole;
+    } else {
+	index = whole + (remainder - .5) * 2; 
+	//log(index);
+    }
+    var splines = interpolate(words, index);
+    draw(splines);
+    
+}
+
+//Given a set of sets of splines and a float indicating where in the set to be, interpolates the control points.
+function interpolate(words, position){
+    var count = words.length;
+    if (position <= 0 || isNaN(position)){
+	return words[0];
+    } 
+    if (position >= words.length - 1){
+	return words[words.length - 1];
+    }
+    if (position == position.toFixed(0) ) {
+	return words[position];
+    }
+    splines = [];
+    from = words[Math.floor(position)];
+    to = words[Math.ceil(position)];
+    tween = position - Math.floor(position);
+    for (var i = 0; i < from.length; i++){
+	var ftent = from[i];
+	var ttent = to[i];
+	points = [];
+	//do first point cartesian
+	points.push(ctween(ftent[0], ttent[0], tween));
+	
+	//do remaining points polar
+	for (var j = 0; j < ftent.length - 1; j++){
+	    var polar = ptween(ftent[j], ftent[j+1], ttent[j], ttent[j+1], tween)
+	    var lastpoint = points.slice(-1)[0];
+	    var px = lastpoint[0];
+	    var py = lastpoint[1];
+	    var rr = polar[0];
+	    var rth = polar[1];
+	    point = [
+		px + Math.cos(rth) * rr,
+		py + Math.sin(rth) * rr
+	    ];
+	    points.push(point);
+	}
+
+	//2q1wwwwujjjjhy``````tgrrrrrrrrrrrrrrrrrqN BBBBBB
+	splines.push(points);
+    }
+    //asString(splines[0]);
+    return splines;
+}
+
+function ctween(fp, tp, d){
+    return [
+	fp[0] + (tp[0] - fp[0]) * d,
+	fp[1] + (tp[1] - fp[1]) * d
+    ];
+}
+
+//given two lines, and an index between them, returns the r and theta of the intermediate line.
+function ptween(fp0, fp1, tp0, tp1, d){
+    var fx0 = fp0[0];
+    var fy0 = fp0[1];
+    var fx1 = fp1[0];
+    var fy1 = fp1[1];
+    var fdx = fx1 - fx0;
+    var fdy = fy1 - fy0;
+    
+    var fr = Math.sqrt(fdx * fdx + fdy * fdy);
+    var fth = Math.atan2(fdy, fdx);
+
+    var tx0 = tp0[0];
+    var ty0 = tp0[1];
+    var tx1 = tp1[0];
+    var ty1 = tp1[1];
+    var tdx = tx1 - tx0;
+    var tdy = ty1 - ty0;
+    
+    var tr = Math.sqrt(tdx * tdx + tdy * tdy);
+    var tth = Math.atan2(tdy, tdx);
+
+    var rr = fr + (tr - fr) * d;
+    if (tth - fth > Math.PI) {
+	tth -= 2 * Math.PI;
+    }
+    if (tth - fth < -1 * Math.PI) {
+	tth += 2 * Math.PI;
+    }
+    var rth = fth + (tth - fth) * d;
+    var deg = 180 / Math.PI;
+    //asString([fth * deg, tth * deg, rth * deg]);
+    return [rr, rth];
+}
+
 function draw(splines){
     var c = document.getElementById("rik_win");
     var ctx = c.getContext("2d");
+    ctx.clearRect(0, 0, 300, 300);
+
+    ctx.save();
     ctx.translate(100, 100);
     ctx.scale(1,-1);
     
@@ -40,10 +146,12 @@ function draw(splines){
 	    ctx.bezierCurveTo(x1,y1,x2,y2,x3,y3);
 	    ctx.stroke();
 	    //asString(p0);
-	    //asString(points);
 	}
-	
+	//asString(points);
     }
+    //asString(splines[0]);
+    ctx.restore();
+    
 }
 
 // Given a word and the riklang data, returns a list of splines
@@ -96,5 +204,7 @@ function log(message){
 }
 
 function asString(object){
-    log(JSON.stringify(object));
+    var string = JSON.stringify(object);
+    
+    log(string.replace(/\.(\d{4})\d+/g, '.$1'));
 }
