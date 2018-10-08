6291088eabf42fec38bd245ea7e83dae31aec48d
Marc Moskowitz <marc@suberic.net>
2018-10-07 23:18:51 -0400

    Removing old java code.

--- parsers/animator/java/src/net/suberic/rikchik/language/GrammarException.java
+++
@@ -1,9 +0,0 @@
-package net.suberic.rikchik.language;
-
-public class GrammarException extends Exception {
-
-    public GrammarException (String message){
-	super (message);
-    }
-
-}
--- parsers/animator/java/src/net/suberic/rikchik/language/display/shape/AbstractShape.java
+++
@@ -1,9 +0,0 @@
-package net.suberic.rikchik.language.display.shape;
-
-import net.suberic.rikchik.language.display.shape.Shape;
-
-public abstract class AbstractShape implements Shape {
-
-    
-
-}
