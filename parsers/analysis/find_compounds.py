981af1afd18ba507ea25beb0c3fe76bf189b8e5c
Marc Moskowitz <marc@suberic.net>
2026-06-04 22:02:53 -0400

    Handling all corpus

--- parsers/analysis/find_compounds.py
+++ parsers/analysis/find_compounds.py
@@ -22,14 +22,31 @@ def convert_corpus():
             if filename.endswith('.yaml'):
                 yaml_files.append(location[0] + '/' + filename)
 
+    yaml_filename_regex = r'../../data/corpus/(....)/(.*).yaml'
+    json_filename_regex = r'/tmp/corpus_\1_\2.json'
     jsonize_template = "yq -o json %(yaml_filename)s > %(json_filename)s"
     for yaml_filename in yaml_files:
-        json_filename = re.sub(r'../../data/corpus/(....)/(.*).yaml', r'/tmp/corpus_\1_\2.json', yaml_filename)
-        jsonize_map =  {'json_filename': json_filename, 'yaml_filename': yaml_filename}
-        jsonize_command =  jsonize_template % jsonize_map
-        os.system(jsonize_command)
-        json_files.append(json_filename)
+        file_count = count_yaml_objects(yaml_filename)
+        if (file_count == 0):
+            json_filename = re.sub(yaml_filename_regex, json_filename_regex, yaml_filename)
+            jsonize_map =  {'json_filename': json_filename, 'yaml_filename': yaml_filename}
+            jsonize_command =  jsonize_template % jsonize_map
+            os.system(jsonize_command)
+            json_files.append(json_filename)
+        else:
+            print ("Multiple files not supported")
+            
     return json_files
+
+
+def count_yaml_objects(filename):
+    count = 0
+    with open(filename, 'r') as fh:
+        for line in fh:
+            if (line.startswith('---')):
+                count += 1
+    return count
+
 
 """
 Turn a json filename into a python object
